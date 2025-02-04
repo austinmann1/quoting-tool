d12cd7567b6cb11e11479cbab6716399a990a0f3
austinmann0011 <austinmann0011@gmail.com>
2025-02-04 01:11:11 -0800

    Add student account and fix table alignments

--- config-overrides.js
+++ config-overrides.js
@@ -1,21 +1,30 @@
 const webpack = require('webpack');
 
 module.exports = function override(config, env) {
+  // Add fallback for node core modules
   config.resolve.fallback = {
+    ...config.resolve.fallback,
+    "stream": require.resolve("stream-browserify"),
+    "buffer": require.resolve("buffer"),
     "crypto": require.resolve("crypto-browserify"),
     "querystring": require.resolve("querystring-es3"),
-    "stream": require.resolve("stream-browserify"),
-    "buffer": require.resolve("buffer/"),
     "timers": require.resolve("timers-browserify"),
-    "vm": require.resolve("vm-browserify")
+    "url": require.resolve("url"),
+    "util": require.resolve("util"),
+    "assert": require.resolve("assert"),
+    "http": require.resolve("stream-http"),
+    "https": require.resolve("https-browserify"),
+    "os": require.resolve("os-browserify/browser"),
+    "path": require.resolve("path-browserify"),
   };
 
+  // Add buffer plugin
   config.plugins = [
+    ...config.plugins,
     new webpack.ProvidePlugin({
+      Buffer: ['buffer', 'Buffer'],
       process: 'process/browser',
-      Buffer: ['buffer', 'Buffer']
     }),
-    ...config.plugins
   ];
 
   return config;
--- craco.config.js
+++ craco.config.js
@@ -5,26 +5,28 @@ module.exports = {
     configure: {
       resolve: {
         fallback: {
+          "querystring": require.resolve("querystring-es3"),
+          "buffer": require.resolve("buffer/"),
+          "url": require.resolve("url/"),
           "stream": require.resolve("stream-browserify"),
           "http": require.resolve("stream-http"),
           "https": require.resolve("https-browserify"),
-          "os": require.resolve("os-browserify/browser"),
-          "url": require.resolve("url/"),
-          "buffer": require.resolve("buffer/"),
-          "process": require.resolve("process/browser"),
+          "zlib": require.resolve("browserify-zlib"),
           "path": require.resolve("path-browserify"),
-          "util": require.resolve("util/"),
-          "querystring": require.resolve("querystring-es3"),
           "crypto": require.resolve("crypto-browserify"),
-          "assert": require.resolve("assert/"),
-          "zlib": require.resolve("browserify-zlib")
+          "util": require.resolve("util/"),
+          "timers": require.resolve("timers-browserify"),
+          "process": require.resolve("process/browser"),
+          "fs": false,
+          "net": false,
+          "tls": false
         }
       },
       plugins: [
         new webpack.ProvidePlugin({
           process: 'process/browser',
-          Buffer: ['buffer', 'Buffer']
-        })
+          Buffer: ['buffer', 'Buffer'],
+        }),
       ]
     }
   }
