14de290a7cc14cc3aef78e5aa9f613769d0c511c
austinmann0011 <austinmann0011@gmail.com>
2025-02-03 22:26:33 -0800

    Add vm-browserify polyfill for Vercel build

--- config-overrides.js
+++ config-overrides.js
@@ -16,6 +16,8 @@ module.exports = function override(config, env) {
     "https": require.resolve("https-browserify"),
     "os": require.resolve("os-browserify/browser"),
     "path": require.resolve("path-browserify"),
+    "vm": require.resolve("vm-browserify"),
+    "zlib": require.resolve("browserify-zlib")
   };
 
   // Add buffer plugin
