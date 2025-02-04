d1f0975209dfbdb28ac4037268d748e316f15ea8
austinmann0011 <austinmann0011@gmail.com>
2025-02-03 23:53:27 -0800

    Simplify webpack config to minimal requirements

--- config-overrides.js
+++ config-overrides.js
@@ -1,22 +1,13 @@
 const webpack = require('webpack');
 
 module.exports = function override(config, env) {
-  // Add minimal polyfills for node modules
   config.resolve.fallback = {
-    "stream": require.resolve("stream-browserify"),
-    "http": require.resolve("stream-http"),
-    "https": require.resolve("https-browserify"),
-    "os": require.resolve("os-browserify/browser"),
-    "url": require.resolve("url/"),
-    "buffer": require.resolve("buffer/"),
-    "process": require.resolve("process/browser"),
-    "path": require.resolve("path-browserify"),
-    "util": require.resolve("util/"),
+    "crypto": require.resolve("crypto-browserify"),
     "querystring": require.resolve("querystring-es3"),
-    "crypto": require.resolve("crypto-browserify")
+    "stream": require.resolve("stream-browserify"),
+    "buffer": require.resolve("buffer/")
   };
 
-  // Add essential providers
   config.plugins = [
     new webpack.ProvidePlugin({
       process: 'process/browser',
